66d3b321750a139310992fb0c67e4785400327e2
Abhay Mehta <44396502+abhayrockks@users.noreply.github.com>
2024-05-18 13:32:31 +0530

    register and login with local storage

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export class Footer extends Component {
+  render() {
+    return (
+      <footer>Copyright Event. All Rights Reserved</footer>
+    )
+  }
+}
+
+export default Footer;
